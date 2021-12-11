5552857ea0929eb9c5744ec2caedcc28d8835f91
Lantern Nassi <Lanternnassi@gmail.com>
2021-12-11 16:56:46 +0300

    details

--- Multix_business/settings.py
+++ Multix_business/settings.py
@@ -182,35 +182,35 @@ StATICFILES_DIRS = [ os.path.join(BASE_DIR , "static") ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-CLOUDINARY_STORAGE = {
-   'CLOUD_NAME': 'nnana',
-   'API_KEY': '295256691468576',
-   'API_SECRET': 'QylwLM68WEPocm00emu3aZNqh_A',
-}
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+# CLOUDINARY_STORAGE = {
+#    'CLOUD_NAME': 'nnana',
+#    'API_KEY': '295256691468576',
+#    'API_SECRET': 'QylwLM68WEPocm00emu3aZNqh_A',
+# }
+# DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, "Media")
 # MEDIA_URL = "/Media/"
 
 # Aws config
 
-# AWS_LOCATION = 'static'
-# AWS_ACCESS_KEY_ID ='AKIAZPSDYV63VD6XYDLO' 
-# AWS_SECRET_ACCESS_KEY = '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'
-# AWS_STORAGE_BUCKET_NAME ='multixapp'
-# AWS_S3_CUSTOM_DOMAIN='%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-# AWS_S3_OBJECT_PARAMETERS = {    
-#      'CacheControl': 'max-age=86400',
-# }
-# DEFAULT_FILE_STORAGE = 'app.storage_backends.MediaStorage'
-# STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+AWS_LOCATION = 'static'
+AWS_ACCESS_KEY_ID ='AKIAZPSDYV63VD6XYDLO' 
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME ='multapp'
+AWS_S3_CUSTOM_DOMAIN='%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {    
+     'CacheControl': 'max-age=86400',
+}
+DEFAULT_FILE_STORAGE = 'Multix_business.storage_backends.MediaStorage'
+#STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
 # ] 
-# STATIC_URL='https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-# ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+#STATIC_URL='https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 # STATICFILES_FINDERS = (           
 #     'django.contrib.staticfiles.finders.FileSystemFinder',    
 #     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 # )
-# AWS_DEFAULT_ACL = None
+AWS_DEFAULT_ACL = None
